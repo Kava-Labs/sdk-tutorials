8f7f7be0215cd832e96cf86963d7d6c81a14c776
John Maheswaran <john@kava.io>
2020-02-18 10:39:41 -0500

    adding a custom error handler for names that do not exist

--- nameservice-j/x/nameservice/internal/types/errors.go
+++ nameservice-j/x/nameservice/internal/types/errors.go
@@ -4,8 +4,7 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// TODO: Fill out some custom errors for the module
-// You can see how they are constructed below:
-// var (
-//	ErrInvalid = sdkerrors.Register(ModuleName, 1, "custom error message")
-// )
+// custom error handler for names that do not exist
+var (
+	ErrNameDoesNotExist = sdkerrors.Register(ModuleName, 1, "name does not exist")
+)
