8c94564818fd4f81f0c28a46eeac2bf1d85dbc4e
John Maheswaran <john@kava.io>
2020-02-19 11:34:58 -0500

    comment out unused functionality that is duplicated in export.go

--- nameservice-j/app/app.go
+++ nameservice-j/app/app.go
@@ -7,7 +7,7 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
 	tmos "github.com/tendermint/tendermint/libs/os"
-	tmtypes "github.com/tendermint/tendermint/types"
+	// tmtypes "github.com/tendermint/tendermint/types"
 	dbm "github.com/tendermint/tm-db"
 
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
@@ -321,21 +321,21 @@ func (app *nameServiceApp) ModuleAccountAddrs() map[string]bool {
 	return modAccAddrs
 }
 
-//_________________________________________________________
+// //_________________________________________________________
 
-func (app *nameServiceApp) ExportAppStateAndValidators(forZeroHeight bool, jailWhiteList []string,
-) (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error) {
+// func (app *nameServiceApp) ExportAppStateAndValidators(forZeroHeight bool, jailWhiteList []string,
+// ) (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error) {
 
-	// as if they could withdraw from the start of the next block
-	ctx := app.NewContext(true, abci.Header{Height: app.LastBlockHeight()})
+// 	// as if they could withdraw from the start of the next block
+// 	ctx := app.NewContext(true, abci.Header{Height: app.LastBlockHeight()})
 
-	genState := app.mm.ExportGenesis(ctx)
-	appState, err = codec.MarshalJSONIndent(app.cdc, genState)
-	if err != nil {
-		return nil, nil, err
-	}
+// 	genState := app.mm.ExportGenesis(ctx)
+// 	appState, err = codec.MarshalJSONIndent(app.cdc, genState)
+// 	if err != nil {
+// 		return nil, nil, err
+// 	}
 
-	validators = staking.WriteValidators(ctx, app.stakingKeeper)
+// 	validators = staking.WriteValidators(ctx, app.stakingKeeper)
 
-	return appState, validators, nil
-}
+// 	return appState, validators, nil
+// }
